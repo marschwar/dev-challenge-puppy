2e5ca85a3e4d40754f305070f5d26a6d6b6a7094
Markus Schwarz <post@markus-schwarz.net>
2021-03-01 12:23:46 +0100

    apply studio template

---
+++ app/src/main/java/de/codekenner/puppy/MainActivity.kt
@@ -0,0 +1,38 @@
+package de.codekenner.puppy
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import de.codekenner.puppy.ui.theme.PuppyTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PuppyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PuppyTheme {
+        Greeting("Android")
+    }
+}

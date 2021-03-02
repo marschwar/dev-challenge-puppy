544276ce6015a68d73364a3a4c7936387cbaf512
Markus Schwarz <post@markus-schwarz.net>
2021-03-02 21:50:21 +0100

    add cross fade

--- app/src/main/java/de/codekenner/puppy/MainActivity.kt
+++ app/src/main/java/de/codekenner/puppy/MainActivity.kt
@@ -4,28 +4,19 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import de.codekenner.puppy.ui.theme.PuppyTheme
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel: PuppyViewModel by viewModels()
 
+    @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -35,14 +26,16 @@ class MainActivity : AppCompatActivity() {
                     val observeAsState = viewModel.viewState.observeAsState()
 
                     observeAsState.value?.let { viewState ->
-                        when (viewState) {
-                            is PuppyList -> PuppyListScreen(
-                                viewState,
-                                onViewEvent = viewModel::onViewEvent
-                            )
-                            is PuppyDetails -> PuppyDetailsScreen(
-                                puppyDetails = viewState
-                            )
+                        Crossfade(targetState = viewState, animationSpec = tween(1000)) { state ->
+                            when (state) {
+                                is PuppyList -> PuppyListScreen(
+                                    state,
+                                    onViewEvent = viewModel::onViewEvent
+                                )
+                                is PuppyDetails -> PuppyDetailsScreen(
+                                    puppyDetails = state
+                                )
+                            }
                         }
                     }
                 }
